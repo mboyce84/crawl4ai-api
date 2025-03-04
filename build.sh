c45c7673abd9379247c469b5ff21c56bc3bb0306
mboyce84 <mboyce84@gmail.com>
2025-03-03 20:26:48 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 set -e  # Exit on first error
-pip install -r requirements.txt  # Install Python dependencies
-python -m playwright install --with-deps  # Install Playwright & dependencies
+
+# Install Python dependencies
+pip install -r requirements.txt
+
+# Install Playwright Browsers *without requiring root access*
+PLAYWRIGHT_BROWSERS_PATH=$(pwd)/ms-playwright python -m playwright install chromium
